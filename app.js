3878b897277fe76e44ab941a5fc07688b5193327
z0f0rt <greenboots@bk.ru>
2023-02-06 22:23:19 +0300

    start working app

---
+++ app.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const AutoLoad = require("@fastify/autoload");
+
+const fastify = require("fastify")({
+  logger: true,
+});
+
+fastify.get("/", function (request, reply) {
+  reply.send({ hello: "world" });
+});
+
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
+
+module.exports = async function (fastify, opts) {
+  // Place here your custom code!
+
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, "plugins"),
+    options: Object.assign({}, opts),
+  });
+
+  // This loads all plugins defined in routes
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, "routes"),
+    options: Object.assign({}, opts),
+  });
+};
+console.log(1);
